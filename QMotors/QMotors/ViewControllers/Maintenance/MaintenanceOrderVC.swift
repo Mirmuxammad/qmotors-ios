64a8ce029535edb9ba7e740f4839666aae0a3d03
Reyganstorm <ruslan.shtybaev@gmail.com>
2022-09-08 11:18:34 +0300

    setup dropDown for cars in free diagnostics

--- QMotors/QMotors/ViewControllers/Maintenance/MaintenanceOrderVC.swift
+++ QMotors/QMotors/ViewControllers/Maintenance/MaintenanceOrderVC.swift
@@ -7,8 +7,15 @@
 
 import UIKit
 import SnapKit
+import DropDown
 
 class MaintenanceOrderVC: BaseVC, UITextFieldDelegate {
+    
+    private var myCars = [MyCarModel]()
+    private var order = NewOrder()
+    private var myCarOrder = MyCarOrder()
+    
+    var myCar: MyCarModel?
     
     // MARK: - UI Elements
     private let logoImageView: UIImageView = {
@@ -82,20 +89,48 @@ class MaintenanceOrderVC: BaseVC, UITextFieldDelegate {
         return label
     }()
     
-    private let infoAboutCar: CustomTextField = {
+    private let userCarField: CustomTextField = {
         let field = CustomTextField(placeholder: "Из списка ваших автомобилей")
         return field
     }()
     
+    private let userCarDropDown: DropDown = {
+        let dropDown = DropDown()
+        dropDown.layer.borderWidth = 1
+        dropDown.layer.borderColor = UIColor(hex: "B6B6B6").cgColor
+        dropDown.layer.cornerRadius = 8
+        return dropDown
+    }()
+    
+    private let userCarButton: UIButton = UIButton()
+    
+    private let carModelChevronButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "chevron-icon"), for: .normal)
+        return button
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
         setupConstraints()
         
+        userCarButton.addTarget(self, action: #selector(openDropDown), for: .touchUpInside)
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        loadInfo()
+    }
+    
+    
     // MARK: - Private actions
+    
+    @objc private func openDropDown() {
+        userCarDropDown.show()
+        carModelChevronButton.transform = CGAffineTransform(rotationAngle: .pi)
+    }
+    
     @objc private func backButtonDidTap() {
         print("backButtonDidTap")
         router?.back()
@@ -105,21 +140,87 @@ class MaintenanceOrderVC: BaseVC, UITextFieldDelegate {
         print(#function)
     }
     
+    private func loadInfo() {
+        self.showLoadingIndicator()
+        let dg = DispatchGroup()
+        loadMyCar(dg: dg)
+        updateTableViews(dg: dg)
+    }
+    
+    private func loadMyCar(dg: DispatchGroup) {
+        dg.enter()
+        CarAPI.getMyCars { [weak self] jsonData in
+            guard let self = self else { return }
+            self.myCars = jsonData.filter { $0.status == 0 }
+            dg.leave()
+        } failure: { error in
+            let alert = UIAlertController(title: "Ошибка", message: error?.message, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
+            
+            }))
+            self.present(alert, animated: true, completion: nil)
+            dg.leave()
+        }
+    }
+    
+    private func updateTableViews(dg: DispatchGroup) {
+        dg.notify(queue: .main) {
+            self.setDropDowns()
+            self.dismissLoadingIndicator()
+        }
+    }
+    
+    private func setDropDowns() {
+        userCarDropDown.dataSource = myCars.map({ i in
+            i.mark + " " + i.model + " " + i.number
+        })
+        userCarDropDown.anchorView = userCarButton
+        userCarDropDown.direction = .bottom
+        userCarDropDown.bottomOffset = CGPoint(x: 0, y:userCarButton.frame.height + 10)
+        userCarDropDown.width = userCarButton.frame.width
+        
+        userCarDropDown.selectionAction = { [weak self] (index: Int, item: String) in
+            self?.userCarField.text = item
+            guard let id = self?.myCars[index].id else { return }
+            guard let myCar = self?.myCars[index] else { return }
+            self?.myCar = myCar
+            self?.order.carId = String(id)
+            self?.order.carNumber = self?.myCars[index].number
+            self?.userCarDropDown.hide()
+            self?.carModelChevronButton.transform = .identity
+            
+            self?.myCarOrder.id = id
+            self?.myCarOrder.carModelId = self?.myCars[index].car_model_id
+            self?.myCarOrder.year = self?.myCars[index].year
+            guard let mileage = Int(self?.myCars[index].mileage ?? "") else { return }
+            self?.myCarOrder.mileage = mileage
+            self?.myCarOrder.vin = self?.myCars[index].vin
+            self?.myCarOrder.number = self?.myCars[index].number
+            self?.myCarOrder.status = CarStatus.active
+        }
+    }
+    
     private func setupView() {
+        userCarField.inputView = UIView()
+        userCarField.delegate = self
         view.addSubview(logoImageView)
         view.addSubview(backgroundView)
         backgroundView.addSubview(backButton)
         backgroundView.addSubview(titleLable)
         backgroundView.addSubview(secondTitleLable)
         backgroundView.addSubview(thirdTitleLable)
-        backgroundView.addSubview(infoAboutCar)
+        
+        backgroundView.addSubview(userCarField)
+        backgroundView.addSubview(userCarButton)
+        userCarField.addSubview(carModelChevronButton)
+        
         backgroundView.addSubview(dateTF)
         backgroundView.addSubview(datePicker)
         backgroundView.addSubview(sendOrderButton)
         
         dateTF.inputView = datePicker
-        infoAboutCar.inputView = UIView()
-        infoAboutCar.delegate = self
+        userCarField.inputView = UIView()
+        userCarField.delegate = self
         
     }
     
@@ -154,15 +255,24 @@ class MaintenanceOrderVC: BaseVC, UITextFieldDelegate {
             make.height.equalTo(22)
         }
         
-        infoAboutCar.snp.makeConstraints { make in
+        userCarField.snp.makeConstraints { make in
             make.top.equalTo(secondTitleLable.snp.bottom).offset(14)
             make.height.equalTo(54)
             make.left.equalToSuperview().offset(20)
             make.right.equalToSuperview().offset(-20)
         }
+        userCarButton.snp.makeConstraints { make in
+            make.edges.equalTo(userCarField)
+        }
+        carModelChevronButton.snp.makeConstraints { make in
+            make.right.equalToSuperview()
+            make.centerY.equalToSuperview()
+            make.height.equalTo(54)
+            make.width.equalTo(54)
+        }
         
         thirdTitleLable.snp.makeConstraints { make in
-            make.top.equalTo(infoAboutCar.snp.bottom).offset(20)
+            make.top.equalTo(userCarField.snp.bottom).offset(20)
             make.height.equalTo(22)
             make.left.equalToSuperview().offset(20)
             make.right.equalToSuperview().offset(-20)
@@ -179,12 +289,4 @@ class MaintenanceOrderVC: BaseVC, UITextFieldDelegate {
             make.right.equalToSuperview().offset(-10)
         }
     }
-    
-    
-    
-}
-
-
-extension MaintenanceOrderVC {
-    
 }
