48d655e0679a1a28cbfc71969d878e198adbbe39
mavlonergashev <mavlik.exe@gmail.com>
2022-09-21 20:38:03 +0500

    Minimum date for order Record set for now

--- QMotors/QMotors/ViewControllers/OrderRecord/OrderRecordVC.swift
+++ QMotors/QMotors/ViewControllers/OrderRecord/OrderRecordVC.swift
@@ -45,6 +45,7 @@ class OrderRecordVC: BaseVC {
         let picker = UIDatePicker()
         let localeID = Locale.preferredLanguages.first
         picker.locale = Locale(identifier: localeID!)
+        picker.minimumDate = Date()
         return picker
     }()
     
